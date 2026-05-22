bccc7960677813a31ab4d2fff464532500247f16
Malinovsky553 <159619729+Malinovsky553@users.noreply.github.com>
2026-05-22 15:07:19 +0000

    final

--- main.py
+++ main.py
@@ -1,23 +1,99 @@
 
-#1 общ
+# #1 общ
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+
+# from sklearn.datasets import make_blobs
+# from sklearn.cluster import KMeans
+# import pandas as pd
+
+# dataset, classes = make_blobs(n_samples=200, n_features=2,
+#                               centers=4, cluster_std=0.5, random_state=0)
+# df = pd.DataFrame(dataset, columns=['var1', 'var2'])
+# print(df.head(2))
+
+# inertias = []
+# k_range = range(1, 12)
+# for k in k_range:
+#     model = KMeans(n_clusters=k, random_state=0)
+#     model.fit(df)
+#     inertias.append(model.inertia_)
+
+# plt.figure()
+# plt.plot(list(k_range), inertias, 'bo-')
+# plt.xlabel('k')
+# plt.ylabel('Distortion score')
+# plt.title('Elbow Method')
+# plt.savefig('elbow_plot.png')
+# plt.close() 
+# print("Elbow plot saved as elbow_plot.png")
+
+# kmeans = KMeans(n_clusters=4, init='k-means++',
+# random_state=0).fit(df)
+# print (kmeans.labels_)
+# print (kmeans.cluster_centers_)
+# print(kmeans.inertia_)
+# print(kmeans.n_iter_)
+
+# from collections import Counter
+# Counter(kmeans.labels_)
+# print(Counter(kmeans.labels_))
+
+# import seaborn as sns
+
+# sns.scatterplot(data=df, x='var1', y='var2', hue=kmeans.labels_)
+# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker="X", c="r", s=80, label="centroids")
+# plt.grid(True)
+# plt.savefig('scatter_plot.png')
+# plt.close() 
+# print("Scatter plot saved as scatter_plot.png") 
+
+
+# инд 4 вариант 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-from sklearn.datasets import make_blobs
-from sklearn.cluster import KMeans
+import numpy as np
 import pandas as pd
 
-dataset, classes = make_blobs(n_samples=200, n_features=2,
-                              centers=4, cluster_std=0.5, random_state=0)
-df = pd.DataFrame(dataset, columns=['var1', 'var2'])
+from sklearn.cluster import KMeans
+from collections import Counter
+import seaborn as sns
+
+# Генерация данных по варианту 4
+np.random.seed(0)
+n_samples = 200
+
+param1 = np.random.uniform(-10, 1, n_samples)
+param2 = np.random.uniform(1, 2, n_samples)
+param3_text = np.random.choice(['отрицательное значение', 'положительное значение'], size=n_samples)
+
+# Переводим текст в числа
+mapping = {
+    'отрицательное значение': 0,
+    'положительное значение': 1
+}
+param3_num = [mapping[x] for x in param3_text]
+
+df = pd.DataFrame({
+    'var1': param1,
+    'var2': param2,
+    'var3_text': param3_text,
+    'var3_num': param3_num
+})
+
 print(df.head(2))
+
+X = df[['var1', 'var2', 'var3_num']]
 
 inertias = []
 k_range = range(1, 12)
+
 for k in k_range:
-    model = KMeans(n_clusters=k, random_state=0)
-    model.fit(df)
+    model = KMeans(n_clusters=k, random_state=0, n_init=10)
+    model.fit(X)
     inertias.append(model.inertia_)
 
 plt.figure()
@@ -25,31 +101,27 @@ plt.plot(list(k_range), inertias, 'bo-')
 plt.xlabel('k')
 plt.ylabel('Distortion score')
 plt.title('Elbow Method')
-plt.savefig('elbow_plot.png')
-plt.close() 
+plt.savefig('var4elbow_plot.png')
+plt.close()
 print("Elbow plot saved as elbow_plot.png")
 
-kmeans = KMeans(n_clusters=4, init='k-means++',
-random_state=0).fit(df)
-print (kmeans.labels_)
-print (kmeans.cluster_centers_)
+kmeans = KMeans(n_clusters=4, init='k-means++', random_state=0, n_init=10).fit(X)
+
+print(kmeans.labels_)
+print(kmeans.cluster_centers_)
 print(kmeans.inertia_)
 print(kmeans.n_iter_)
-
-from collections import Counter
-Counter(kmeans.labels_)
 print(Counter(kmeans.labels_))
 
-import seaborn as sns
-
+plt.figure()
 sns.scatterplot(data=df, x='var1', y='var2', hue=kmeans.labels_)
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker="X", c="r", s=80, label="centroids")
 plt.grid(True)
-plt.savefig('scatter_plot.png')
-plt.close() 
-print("Scatter plot saved as scatter_plot.png") 
-
-# #2 инд 3 вариант
+plt.legend(loc='upper right')
+plt.savefig('var4scatter_plot.png')
+plt.close()
+print("Scatter plot saved as scatter_plot.png")
+#  инд 3 вариант
 # Подключение библиотек
 # import matplotlib
 # matplotlib.use('Agg')
@@ -141,5 +213,144 @@ print("Scatter plot saved as scatter_plot.png")
 # plt.grid(True)
 # plt.legend(loc='upper right')
 # plt.savefig('var3scatter_plot.png')
+# plt.close()
+# print("Scatter plot saved as scatter_plot.png")
+
+
+# #2 вариант
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+
+# import numpy as np
+# import pandas as pd
+
+# from sklearn.cluster import KMeans
+# from collections import Counter
+# import seaborn as sns
+
+# # Генерация данных по варианту 2
+# np.random.seed(0)
+# n_samples = 200
+
+# param1 = np.random.uniform(0.01, 1, n_samples)
+# param2 = np.random.uniform(1, 300, n_samples)
+# param3_text = np.random.choice(['Самара', 'Тольятти', 'Москва'], size=n_samples)
+
+# # Переводим города в числа
+# mapping = {'Самара': 1, 'Тольятти': 2, 'Москва': 3}
+# param3_num = [mapping[x] for x in param3_text]
+
+# df = pd.DataFrame({
+#     'var1': param1,
+#     'var2': param2,
+#     'var3_text': param3_text,
+#     'var3_num': param3_num
+# })
+
+# print(df.head(2))
+
+# X = df[['var1', 'var2', 'var3_num']]
+
+# inertias = []
+# k_range = range(1, 12)
+
+# for k in k_range:
+#     model = KMeans(n_clusters=k, random_state=0, n_init=10)
+#     model.fit(X)
+#     inertias.append(model.inertia_)
+
+# plt.figure()
+# plt.plot(list(k_range), inertias, 'bo-')
+# plt.xlabel('k')
+# plt.ylabel('Distortion score')
+# plt.title('Elbow Method')
+# plt.savefig('var2elbow_plot.png')
+# plt.close()
+# print("Elbow plot saved as elbow_plot.png")
+
+# kmeans = KMeans(n_clusters=3, init='k-means++', random_state=0, n_init=10).fit(X)
+
+# print(kmeans.labels_)
+# print(kmeans.cluster_centers_)
+# print(kmeans.inertia_)
+# print(kmeans.n_iter_)
+# print(Counter(kmeans.labels_))
+
+# plt.figure()
+# sns.scatterplot(data=df, x='var1', y='var2', hue=kmeans.labels_)
+# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker="X", c="r", s=80, label="centroids")
+# plt.grid(True)
+# plt.legend(loc='upper right')
+# plt.savefig('var2scatter_plot.png')
+# plt.close()
+# print("Scatter plot saved as scatter_plot.png")
+
+# #1 вариант
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+
+# import numpy as np
+# import pandas as pd
+
+# from sklearn.cluster import KMeans
+# from collections import Counter
+# import seaborn as sns
+
+# # Генерация данных по варианту 1
+# np.random.seed(0)
+# n_samples = 200
+
+# param1 = np.random.uniform(0.1, 5, n_samples)
+# param2 = np.random.uniform(0.1, 3, n_samples)
+# param3_text = np.random.choice(['10%', '20%', '80%', '90%'], size=n_samples)
+
+# # Переводим проценты в числа
+# mapping = {'10%': 10, '20%': 20, '80%': 80, '90%': 90}
+# param3_num = [mapping[x] for x in param3_text]
+
+# df = pd.DataFrame({
+#     'var1': param1,
+#     'var2': param2,
+#     'var3_text': param3_text,
+#     'var3_num': param3_num
+# })
+
+# print(df.head(2))
+
+# X = df[['var1', 'var2', 'var3_num']]
+
+# inertias = []
+# k_range = range(1, 12)
+
+# for k in k_range:
+#     model = KMeans(n_clusters=k, random_state=0, n_init=10)
+#     model.fit(X)
+#     inertias.append(model.inertia_)
+
+# plt.figure()
+# plt.plot(list(k_range), inertias, 'bo-')
+# plt.xlabel('k')
+# plt.ylabel('Distortion score')
+# plt.title('Elbow Method')
+# plt.savefig('var1elbow_plot.png')
+# plt.close()
+# print("Elbow plot saved as elbow_plot.png")
+
+# kmeans = KMeans(n_clusters=4, init='k-means++', random_state=0, n_init=10).fit(X)
+
+# print(kmeans.labels_)
+# print(kmeans.cluster_centers_)
+# print(kmeans.inertia_)
+# print(kmeans.n_iter_)
+# print(Counter(kmeans.labels_))
+
+# plt.figure()
+# sns.scatterplot(data=df, x='var1', y='var2', hue=kmeans.labels_)
+# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker="X", c="r", s=80, label="centroids")
+# plt.grid(True)
+# plt.legend(loc='upper right')
+# plt.savefig('var1scatter_plot.png')
 # plt.close()
 # print("Scatter plot saved as scatter_plot.png")
